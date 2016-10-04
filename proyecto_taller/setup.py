2ec37b933c7e7f9ca0e7e73fb1ceef4fcb24d432
karlisle <inoue.sora@gmail.com>
2016-10-03 21:47:49 -0500

    El esqueleto del proyecto del tercer día

---
+++ proyecto_taller/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='proyecto_taller',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='GPL',
+    author='kharlvaron',
+    author_email='kharlvaron@gmail.com',
+    description='Un taller sobre OpenCV 3.1 y Pyton3.4'
+)
